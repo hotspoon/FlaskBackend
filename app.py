0cf1b6e1257f234b2b757d8c9fbb8d6eb69228a2
hotspoon <fariez18@outlook.com>
2020-07-09 20:24:30 +0700

    proses glcm uploaded image

--- app.py
+++ app.py
@@ -30,16 +30,68 @@ def classification():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            return json.dumps({ 'message' : 'no selected file' })
+            return json.dumps({ 'message' : 'No selected file' })
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            return json.dumps({ 'message' : 'no selected file' })
+            return json.dumps({ 'message' : 'No selected file' })
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return json.dumps({ 'message' : 'success' })
+            glcm_one_image(file)
+            return json.dumps({ 'message' : 'Success' })
+
+def glcm_one_image(file):
+    contrast = []
+    energy = []
+    homo = []
+    corr = []
+    filename = []
+    label = []
+
+    img = cv.imread(UPLOAD_FOLDER + '/' + file.filename)
+    print(img)
+    filename.append(file.filename)
+    label.append(None)
+
+    # ==== resize image =======
+    print(file)
+    dim = (96, 96)
+    img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+
+    # ==== remove background grabcut=====
+    mask = np.zeros(img.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    rect = (1,1,290,290)
+    cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    img = img*mask2[:,:,np.newaxis]
+
+    # ==== grayscale ====
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # ==== save new image =====
+    cv.imwrite('result/image_gray/' + file.filename, gray)
+
+    # ===== GLCM ====
+    glcm = greycomatrix(gray, [5], [0], 256, symmetric=True, normed=True)
+    contrast.append(greycoprops(glcm, 'contrast')[0, 0])
+    energy.append(greycoprops(glcm, 'energy')[0, 0])
+    homo.append(greycoprops(glcm, 'homogeneity')[0, 0])
+    corr.append(greycoprops(glcm, 'correlation')[0, 0])
+
+    df = pd.DataFrame()
+    df['filename'] = filename
+    df['contrast'] = contrast
+    df['energy'] = energy
+    df['homo'] = homo
+    df['corr'] = corr
+    df['label'] = label
+
+    df.to_csv('result/csv_test/test_csv.csv')
+
 
 def glcm(filename):
     contrast = []
